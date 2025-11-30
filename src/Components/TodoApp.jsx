f58bc17ffd2bd5c839650b1b503d54ae72dce555
Takeshi Watanabe <takeshi.watanabe@live.com>
2025-12-01 00:18:50 +0900

    adicionando db.json, e Toggle para alterar os status do todo

--- src/Components/TodoApp.jsx
+++ src/Components/TodoApp.jsx
@@ -1,43 +1,105 @@
 
 import { useEffect, useState } from 'react'
-const KEY = "KEY"
 //useEffect para pegar informaoes ao iniciar o programa
-
+const URL = "http://localhost:3004/todos"
 const TodoApp = () => {
 
 useEffect(()=> {
-const fet = localStorage.getItem(KEY);
-if (fet){
-const req = JSON.parse(fet)
-    setTodo(req)
-}},[])
+    const fetchTodo = async ()=>{
+        try{
+        const res = await fetch(URL)
+        if(!res.ok){
+            throw new Error(`falha ao carrregar dados. Status: ${res.status}`)
+        }
+        const data = await res.json();
+        setTodo(data);
+    }catch(error){
+        console.error("erro do servidor", error);
+    }
+   
+    }
+     fetchTodo();
+},[])
         // usestates que vou usar no programa
     const [todo,setTodo] = useState([]);
     const [input, setInput] = useState("");
 
-const handleSubmmit = (e)=>{
-    e.preventDefault()
-    if(input.trim() !== ""){
-        const newTodo = {
-            id: Date.now(),
-            text: input
-        }
-        setTodo((prev)=> [...prev, newTodo]);
-        setInput("")
+const handleSubmmit = async (e)=>{
+    e.preventDefault();
+    try{
+        if(input.trim() !== ""){
+
+    const newTodo = {
+        text: input,
+        done: false
+    }
+
+    const res = await fetch(URL,{
+        method: "POST",
+        headers: {"Content-type":"application/json"},
+        body: JSON.stringify(newTodo)
+    });
+
+    if(!res.ok){
+throw new Error(`Falha no envio: Status ${res.status}`);
+            };
+
+            
+            const createdTodo = await res.json();
+            setTodo((prevTodo)=> ([...prevTodo,createdTodo]))
+           setInput("");
+}}catch(error) {
+        // ✅ A CORREÇÃO: Bloco catch obrigatório para lidar com erros de rede ou o 'throw'
+        console.error("Houve um erro na requisição ou no servidor:", error);
     }
 }
-const handleDeletar = (id) => {
-   const atualizado = todo.filter((todo)=> {
-    return todo.id !== id
-    })
-    setTodo(atualizado)
+
+
+const handleDeletar = async (id) => {   
+    const urlID = `${URL}/${id}`;
+    try{
+        const res = await fetch(urlID,{
+        method: "DELETE"
+    }); 
+    if(res.ok){
+        setTodo(
+            (prevTodo)=> prevTodo.filter((task)=> task.id !== id)
+        )
+    }else{
+        throw new Error(`falha ao deletar a tarefa! Status ${res.status}`);
+    }}catch (error) {
+        console.error("Um erro ocorreu na requisição:", error);
+        
+    }
 }
-useEffect(()=>{
-    //salvar dados enviados da todo para o localstorage
-    if(todo.length > 0){
-    const task = JSON.stringify(todo)
-localStorage.setItem(KEY, task)
-}},[todo])
+const handleToggle = async (id,statusAtual) => {
+        const novoStatus = !statusAtual;
+        const urlID = `${URL}/${id}`;
+        try{
+            const res = await fetch(urlID,{
+                method: "PATCH",
+                headers: {"Content-type":"application/json"},
+                body: JSON.stringify({done: novoStatus})
+            });
+            if(!res.ok){
+                throw new Error(`falhou, erro: ${res.status}`)
+            }const updatedTask = await res.json();
+            // atualizar a lista dos todos com a nova done: que atualizei no backend
+            setTodo(
+                (prevTodo)=>{
+                   return prevTodo.map((task)=>{
+                        if(task.id === id){
+                        return updatedTask}
+                        return task;
+                    })
+                }
+            )
+            
+        }catch (error) {
+            console.error("erro ao executar a tarefa", error);
+        }
+    }
+    
 
   return (
 
@@ -57,6 +119,7 @@ localStorage.setItem(KEY, task)
     {todo.map((task)=>(
          <li key={task.id}>
             {task.text}
+            <button onClick={() => handleToggle(task.id, task.done)}>FEITA</button>
             <button onClick={() => handleDeletar(task.id)}>DELETAR</button>
          </li>)
         
