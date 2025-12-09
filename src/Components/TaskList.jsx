9b8b4f150c88db6fe3213f761a6f891797eb6f6d
Takeshi Watanabe <takeshi.watanabe@live.com>
2025-12-09 23:23:12 +0900

    tailwindCSS

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -6,11 +6,11 @@ const TaskList = ({todo, handleDeletar, handleToggle}) => {
         <ul>
         {todo.map((task)=>(
           
-         <li key={task.id} className='p-5 bg-neutral-50 border rounded-xl m-2.5  w-auto shadow-x1 flex justify-between hover:bg-white'>
+         <li key={task.id} className='p-10 bg-neutral-50 border rounded-xl m-10  w-auto shadow-x1 flex justify-between hover:bg-white shadow-xl/30'>
             {task.done? <p className="done text-shadow-lg/30">{task.text}</p> : <p className="ndone">{task.text}</p> }
            <div className="flex space-x-2">
-            <button className='justify-items-end border border-neutral-300 hover:bg-neutral-400' onClick={() => handleToggle(task.id, task.done)}>  DONE  </button>
-            <button className='justify-items-end border border-neutral-300 hover:bg-neutral-400' onClick={() => handleDeletar(task.id)}>  DELETE  </button>
+            <button className='justify-items-end  border-neutral-300 p- hover:bg-neutral-400' onClick={() => handleToggle(task.id, task.done)}>  DONE  </button>
+            <button className='justify-items-end  border-neutral-300 p- hover:bg-neutral-400' onClick={() => handleDeletar(task.id)}>  DELETE  </button>
             </div>
          </li>)
         
